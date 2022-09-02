de8ed32ddbbeb4bc77b53c8de5bb258a331e7511
juice <3220ju@naver.com>
2022-09-02 09:01:09 +0900

    add stage

--- smoke.js
+++ smoke.js
@@ -1,12 +1,24 @@
 import http from 'k6/http';
 import { check, group, sleep, fail } from 'k6';
 
-export let options = {
-  vus: 1, // 1 user looping for 1 minute
-  duration: '10s',
+// export let options = {
+//   vus: 1, // 1 user looping for 1 minute
+//   duration: '10s',
+//
+//   thresholds: {
+//     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+//   },
+// };
 
+export let options = {
+  stages: [
+    { duration: '1m', target: 500 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '2m', target: 500 }, // stay at 100 users for 10 minutes
+    { duration: '10s', target: 0 }, // ramp-down to 0 users
+  ],
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
   },
 };
 
